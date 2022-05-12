bcc89377e2f3017e940e80b60c02f7147b5560fe
Isaque Bernardes <isaque.bernardes@sensedia.com>
2022-05-12 18:00:18 -0300

    Initial commit

---
+++ src/main/java/br/com/isaquebrb/customerchallenge/CustomerChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.customerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isaquebrb/customerchallenge/CustomerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaquebrb.customerchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
